aa7f16538c3a76285c7c3a44611c78f7dec79ff3
LewJun <lewjun072@sina.com>
2017-12-29 18:08:42 +0800

    自定义Application基类

--- app/src/main/java/com/lewjun/blog/BaseApplication.java
+++ app/src/main/java/com/lewjun/blog/BaseApplication.java
@@ -2,17 +2,20 @@ package com.lewjun.blog;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 /**
  * 自定义Application基类
  * Created by LewJun on 2017/12/28.
  */
 public class BaseApplication extends Application {
+    private static final String TAG = "BaseApplication";
     private SQLiteDatabase db;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate: ");
         MySQLiteOpenHelper sqLiteOpenHelper = new MySQLiteOpenHelper(this, "mydb", null, 2);
         db = sqLiteOpenHelper.getWritableDatabase();
     }
