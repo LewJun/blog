cfc0c89353597943b6196d65ab6b3eb2cd03d4e9
LewJun <lewjun072@sina.com>
2017-12-29 18:08:59 +0800

    Blog适配器

--- app/src/main/java/com/lewjun/blog/BlogAdapter.java
+++ app/src/main/java/com/lewjun/blog/BlogAdapter.java
@@ -3,28 +3,22 @@ package com.lewjun.blog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 /**
  * Blog适配器
  * Created by LewJun on 2017/12/27.
  */
-public class BlogAdapter extends ArrayAdapter<BlogEntity> {
+public class BlogAdapter extends AbstractBaseAdapter<BlogEntity> {
 
-    private int itemlistBlogRes = 0;
-
-    public BlogAdapter(@NonNull Context context, int resource, @NonNull List<BlogEntity> objects) {
-        super(context, resource, objects);
-        itemlistBlogRes = resource;
+    public BlogAdapter(@NonNull Context context) {
+        super(context);
     }
 
     @NonNull
@@ -34,7 +28,7 @@ public class BlogAdapter extends ArrayAdapter<BlogEntity> {
         View view;
         ViewHolder viewHolder;
         if (convertView == null) {
-            view = LayoutInflater.from(getContext()).inflate(itemlistBlogRes, parent, false);
+            view = layoutInflater.inflate(R.layout.listitem_blog, parent, false);
             viewHolder = new ViewHolder();
             viewHolder.tv_title = view.findViewById(R.id.tv_title);
             viewHolder.tv_pubtime = view.findViewById(R.id.tv_pubtime);
