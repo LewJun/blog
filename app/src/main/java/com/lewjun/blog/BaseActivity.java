4c02897572eaf3d8d42b41641f8a180ed70e391b
LewJun <lewjun072@sina.com>
2017-12-29 18:08:25 +0800

    Activity基类

---
+++ app/src/main/java/com/lewjun/blog/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.lewjun.blog;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity基类
+ * Created by LewJun on 2017/12/29.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
